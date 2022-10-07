6fec6e183ae7492331bcc854e37fe7430b028874
Kalkidan Siyum <kseyoum@gmail.com>
2022-10-07 18:21:32 +0300

    created authentication page component alongside sign in form

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Button from "../button/button.component";
 import FormInput from "../form-input/form-input.compoenent";
 import {
-  createAuthUserFromEmailAndPassword,
   createUserDocFromAuth,
   signinUserWithEmailAndPassword,
   signinWithGooglePopup,
