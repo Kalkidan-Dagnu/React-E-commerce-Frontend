6d06adb2d0865abd0fbabd589aba4642efacae5d
Kalkidan Siyum <kseyoum@gmail.com>
2022-10-24 12:00:13 +0300

    add custom logger

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,24 @@ import { applyMiddleware, compose } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 import { legacy_createStore as createStore} from 'redux'
+import { isSignInWithEmailLink } from "firebase/auth";
 
+const loggerMiddleware = (store) => (next) => (action) => {
+    if(!action.type) {
+    return next(action);
+    }
 
-const middlewares = [logger];
+    console.log('type :' , action.type);
+    console.log('payload :', action.payload);
+    console.log('Current State :', store.getState());
+
+    next(action);
+
+    console.log('next state', store.getState())
+
+}
+
+const middlewares = [loggerMiddleware];
 const composedEnhancers = compose(applyMiddleware(...middlewares))
 
 export const store = createStore(rootReducer,undefined,composedEnhancers)
